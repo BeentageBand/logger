9892d9764ddf9586697e3b4144882ee6790d94f9
Roberto Alanis <roalanis@amazon.com>
2020-02-29 22:26:24 -0800

    rm unit-test and add log levels to the test, ignore debug level

--- tst/logger_test.cc
+++ tst/logger_test.cc
@@ -9,16 +9,22 @@ static char const * const Facility_Name = "Gtest";
 TEST(Logger, Populate)
 {
     LoggerHandler_populate(&LoggerHandler);
-    Formatter_populate(&Formatter, Facility_Name, LOG_DEBUG_LEVEL, "");
+    Formatter_populate(&Formatter, Facility_Name, LOG_INFO_LEVEL, "");
     Logger_populate(&Logger, &Formatter, &LoggerHandler);
 }
 
 TEST(Logger, log_msg)
 {
-    Logger_debug(&Logger, "this is a test");
+    Logger_debug(&Logger, "this is a test that should no appear");
+    Logger_info(&Logger, "this is a test");
+    Logger_warn(&Logger, "this is a test");
+    Logger_error(&Logger, "this is a test");
 }
 
 TEST(Logger, log_msg_with_params)
 {
+    Logger_debug(&Logger, "this is a test num = %d that should no appear", 1);
     Logger_info(&Logger, "this is a test num = %d", 1);
+    Logger_warn(&Logger, "this is a test num = %d", 1);
+    Logger_error(&Logger, "this is a test num = %d", 1);
 }
